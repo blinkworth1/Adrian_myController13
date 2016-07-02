cbe4930e00216677d2866f59005f665b512756da
blinkworth1 <lenny667@gmail.com>
2016-07-02 18:38:43 +1200

    Update myController.cpp

--- myController.cpp
+++ myController.cpp
@@ -165,7 +165,10 @@ void Switches::ReadWrite() {
 }
 
 void Switches::SwitchesRead () {
-	Sobj->switchesRaw = ( ((GPIOB_PDIR & 0x00F) << 9) |      (  ( (GPIOC_PDIR & 0x3FF) >> 1) & (~(1<<4))  )    ) | (digitalReadFast(7) <<4)  ;
+	for (int i = 0; i < Sobj->numberOfSwitches; i++) {
+	Sobj->switchesRaw |= (digitalReadFast(i)  ;
+        Sobj->switchesRaw <<= 1;
+	}	
 }
 
 Rotary::Rotary (uint8_t left, uint8_t right) {
