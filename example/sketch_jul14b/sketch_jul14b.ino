6f069208caa53a602f8145ea70b7742036dacfbc
blinkworth1 <lenny667@gmail.com>
2016-07-14 23:21:42 +1200

    push

---
+++ example/sketch_jul14b/sketch_jul14b.ino
@@ -0,0 +1,34 @@
+#include <myController.h> // that the name of my library
+
+Rotary encoder1 (4, 6); //a new encoder with left and right pins
+
+int rotary1mod = 0;
+int pwm = 8; //pwm pin
+
+void setup() {
+  encoder1.SetHandleLeft(left1);
+  encoder1.SetHandleRight(right1);
+}
+
+void loop() {
+  Rotary::ReadWrite(); // Sets the polling of the encoder in motion.  
+  
+  }
+
+void left1 (void) {
+  rotary1mod ++;
+  if (rotary1mod >= 1023) {
+    rotary1mod = 1023;
+  }
+  analogWrite (pwm, rotary1mod); 
+}
+
+
+void right1 (void) {
+  rotary1mod --;
+  if (rotary1mod >= 0) {
+    rotary1mod = 0;
+  }
+analogWrite (pwm, rotary1mod); 
+}
+
